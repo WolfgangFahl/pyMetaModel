5c549a48e6645127641704e491c013a8c06ce16e
Wolfgang Fahl <wf@bitplan.com>
2024-01-15 07:55:37 +0100

    towards #29

--- meta/metamodel_cmd.py
+++ meta/metamodel_cmd.py
@@ -42,7 +42,7 @@ class MetaModelCmd:
         program_license = """%s
         
           Created by %s on %s.
-          Copyright 2022-2023 Wolfgang Fahl. All rights reserved.
+          Copyright 2022-2024 Wolfgang Fahl. All rights reserved.
         
           Licensed under the Apache License 2.0
           http://www.apache.org/licenses/LICENSE-2.0
